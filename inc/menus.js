57ad49a9a8a65a7dfd11808b75aaa715bfac945f
Pedro <pedro7109.j@gmail.com>
2022-10-21 12:54:19 -0300

    shaping the menu

--- inc/menus.js
+++ inc/menus.js
@@ -1,17 +1,25 @@
 let conn = require('./db');
 
 module.exports = {
-
     getMenus(){
-        return new Promise((resolve, reject) => {
+
+        return new Promise((resolve, reject)=>{
+
             conn.query(`
-            SELECT * FROM tb_menus ORDER BY title
-             `,(err, results)=>{
-                    if(err){
-                    reject(err)
-                    }
-                resolve(results);
-             })
-        })
+                SELECT * FROM tb_menus ORDER BY title
+            `, (err, results)=>{
+
+                if (err) {
+                    reject(err);
+                }
+
+               resolve(results);
+
+            }); 
+
+        });
+
     }
+
+
 }
